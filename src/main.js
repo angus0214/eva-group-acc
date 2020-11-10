0e789c228699bcbdda60f8561f69136432df1ae2
Angus Wu <s0937196597@gmail.com>
2020-11-10 20:49:55 +0800

    set v-calender

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VCalendar from 'v-calendar';
 
 Vue.config.productionTip = false
+
+Vue.use(VCalendar, {
+  componentPrefix: 'vc',  // Use <vc-calendar /> instead of <v-calendar />
+});
 
 new Vue({
   render: h => h(App),
