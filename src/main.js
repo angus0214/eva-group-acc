b0379fcf1fcd764129512ffb052cce14e39d6fff
Angus Wu <s0937196597@gmail.com>
2020-11-10 22:15:12 +0800

    install vue-router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VCalendar from 'v-calendar';
+import router from './router'
 
 Vue.config.productionTip = false
 
@@ -9,5 +10,6 @@ Vue.use(VCalendar, {
 });
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
