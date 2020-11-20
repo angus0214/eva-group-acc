74a13d19ea27a9422d4b9b36b9823a67cd7f0d92
Angus Wu <s0937196597@gmail.com>
2020-11-20 21:14:49 +0800

    set NewPins UI

--- src/views/NewPin.vue
+++ src/views/NewPin.vue
@@ -1,21 +1,20 @@
 <template>
-  <div>
-    new pins
-  </div>
+  <v-row class="justify-center">
+    <v-col cols="12" class="text-center text-h3">
+      Pin Your Event
+    </v-col>
+    <v-col cols="9">
+      <v-text-field label="Url ..." solo></v-text-field>
+    </v-col>
+    <v-col cols="4" class="d-flex justify-center">
+      <v-btn elevation="2" large>Pin!</v-btn>
+    </v-col>
+  </v-row>
 </template>
 <script>
 export default {
   data() {
-    return {
-      newEvent: {
-        name: 'CCSCA SYMPOSIUM：The ‘New Normal’',
-        img: '',
-        date: '2020/11/13',
-        address: '台北市大安區和平東路二段134號',
-        category: '展覽',
-        CountPins: '1',
-      },
-    };
+    return {};
   },
 };
 </script>
